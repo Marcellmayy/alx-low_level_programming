e75f3182bdb78ba391615fc54c6989bdb78c2d94
Marcellmayy <okoubimarcellmay@yahoo.com>
2023-07-14 06:31:28 -0700

    the updated tasks mallocrevised 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int con1 = 0, con2 = 0, i;
-	char *y = NULL;
+	unsigned int cont1 = 0, cont2 = 0, i;
+	char *p = NULL;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[con1] != '\0')
-		con1++;
-	while (s2[con2] != '\0')
-		con2++;
-
-	if (n >= con2)
-		n = con2;
-
-	y = malloc(con1 + n + 1);
-
-	if (!y)
-		return (NULL);
-
-	for (i = 0; i < con1; i++)
 	{
-		y[i] = s1[i];
+		s1 = "";
 	}
-
-	for (i = con1; i < (con1 + n); i++)
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[cont1] != '\0')
+		cont1++;
+	while (s2[cont2] != '\0')
+		cont2++;
+	if (n >= cont2)
+		n = cont2;
+	p = malloc(cont1 + n + 1);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < cont1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (i = cont1; i < (cont1 + n); i++)
 	{
 		p[i] = s2[i - cont1];
 	}
+	p[i] = '\0';
+	return (p);
 
-	y[i] = '\0';
-
-	return (y);
 }
