85ce3178db535923e409540693cf6623aa976993
Marcellmayy <okoubimarcellmay@yahoo.com>
2023-07-19 06:23:14 -0700

    final fix to advanced tasks

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,28 +5,33 @@
 *  @argc: the number of parameters.
 *  @argv: the parameeters in the case the number of bytes.
 *  Description: this program prints opcodes in hexa)
-*  Return: 0 in succes
+*  Return: 0 in success
+*
 */
 
 int main(int argc, char *argv[])
 {
 	int i, n;
-	
-    if (argc != 2) 
+
+	if (argc != 2)
 	{
-        printf("Error\n");
-        return 1;
-    }
-
-    int num_bytes = atoi(argv[1]);
-    if (num_bytes <= 0) 
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
-        printf("Error\n");
-        return 2;
-    }
+		printf("Error\n");
+		exit(2);
+	}
 
-    unsigned char *start = (unsigned char *)&main;
-    print_opcodes(start, num_bytes);
-
-    return 0;
-}
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}}
